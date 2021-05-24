fdeeb59840b14de77fd4a78c6c08a5190950ae9a
gary castillo <garycastle@garys-MacBook-Pro.local>
2021-05-24 13:06:13 -0700

    flasking

--- app.py
+++ app.py
@@ -18,3 +18,33 @@ station = Base.classes.station
 session = Session(engine)
 
 app = Flask(__name__)
+
+@app.route("/")
+def home():
+	print("Homepage")
+	return(f"Climate API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/2016-01-01/<br/>"
+        f"/api/v1.0/2016-01-01/2016-12-31/")
+
+@app.route('/api/v1.0/precipitation/')
+def precipitation():
+
+    last_date = session.query(meas.date).order_by(meas.date.desc()).first().date
+    last_year = dt.datetime.strptime(last_date, '%Y-%m-%d') - dt.timedelta(days=365)
+
+    rain_results = session.query(meas.date, meas.prcp).\
+    filter(meas.date >= last_year).\
+    order_by(meas.date).all()
+
+    p_dict = dict(rain_results)
+    print(f"Results for Precipitation - {p_dict}")
+    print("Out of Precipitation section.")
+    return jsonify(p_dict) 
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
